8e32c7c64c6b7f792d52f40e14cc10a6cb7c4783
puzz13 <puzz13one@gmail.com>
2024-10-19 21:34:36 +0300

    Add draft stack

--- kovtun.alexander/S2/main.cpp
+++ kovtun.alexander/S2/main.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+#include <cassert>
+
+#include "stack.hpp"
+
 int main()
 {
+  kovtun::Stack< int > stack;
+
+  for (int i = 0; i < 10; i++)
+  {
+    stack.push_back(i);
+    stack.pop_back();
+    stack.push_back(i + 1);
+  }
+
+  assert(stack.size() == 10);
+
   return 0;
 }
---
+++ kovtun.alexander/S2/stack.hpp
@@ -0,0 +1,50 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include "../common/list/list.hpp"
+
+namespace kovtun
+{
+  template< typename T >
+  class Stack
+  {
+  public:
+    Stack() = default;
+    ~Stack() = default;
+
+    void push_back(const T & val);
+    void pop_back();
+
+    size_t size() const noexcept;
+    bool empty() const noexcept;
+
+  private:
+    List< T > list_;
+  };
+
+  template< typename T >
+  void Stack< T >::push_back(const T & val)
+  {
+    list_.push_back(val);
+  }
+
+  template< typename T >
+  void Stack< T >::pop_back()
+  {
+    list_.pop_back();
+  }
+
+  template< typename T >
+  size_t Stack< T >::size() const noexcept
+  {
+    return list_.size();
+  }
+
+  template< typename T >
+  bool Stack< T >::empty() const noexcept
+  {
+    return size() == 0;
+  }
+}
+
+#endif
